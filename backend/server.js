77a7679ca289e365e1269e815c7fdff52b7b8f2f
DineshRana716 <dkr9304650@gmail.com>
2025-04-03 22:57:21 +0530

    Update backend with user model, controllers, and routes

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
-require("dotenv").config();
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
 
+dotenv.config();
+
+connectDB();
 const app = express();
-const chats = require("./data");
+app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-  res.send("api is running");
+  res.send("api is running successfully");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  const singleChat = chats.find((chat) => chat._id === req.params.id);
-  res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
